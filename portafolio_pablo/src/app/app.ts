f727354cce36c4a94dca150b653e38605720d488
PabloMartinVR <pablovelascoruiz68@gmail.com>
2026-02-18 13:30:11 +0100

    despliege automatico con extension de angular

--- portafolio_pablo/src/app/app.ts
+++ portafolio_pablo/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('portfolio_pablo');
+  protected readonly title = signal('portafolio_pablo');
 }
